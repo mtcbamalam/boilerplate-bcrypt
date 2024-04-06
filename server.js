4fad50b64038e3729a2078c8b164d665eae3db16
mtc bamalam <mc_bamalam@protonmail.com>
2024-04-06 14:15:23 +0000

    Bcrypt Hash

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
 
-
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 
 
