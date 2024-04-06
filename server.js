dac7e0fdc10cb98cfd41791b31f0fe627fb7e1e4
mtc bamalam <mc_bamalam@protonmail.com>
2024-04-06 16:16:29 +0000

    Synch

--- server.js
+++ server.js
@@ -17,12 +17,16 @@ bcrypt.hash(myPlaintextPassword, saltRounds, (_err, hash) => {
   });
 
 //END_ASYNC
-
-//START_SYNC
 var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
  console.log(hash);
 
 var hash = bcrypt.hashSync(myPlaintextPassword, hash);
+console.log(result);
+//START_SYNC
+
+let hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+console.log(hash);
+let result = bcrypt.compareSync(myPlaintextPassword, hash);
 console.log(result);
 
 //END_SYNC
