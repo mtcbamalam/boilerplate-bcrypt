ab3baf0fde32b9140c3baa592bb260c9d43b3459
mtc bamalam <mc_bamalam@protonmail.com>
2024-04-06 15:18:59 +0000

    Bcrypt Sync

--- newFile.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-const { app } = require('./server.js');
-
-app.get('/', (reg, res) => {
-    res.send("Hello World!");
-});
--- server.js
+++ server.js
@@ -9,9 +9,6 @@ const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
 
-app.get('/', (reg, res) => {
-    res.send("Hello World!");
-});
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
     console.log(hash);
