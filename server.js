f58ba0674638ccaf61ebec822e99f8c2fbaa3378
mtc bamalam <mc_bamalam@protonmail.com>
2024-04-06 14:52:04 +0000

    Bcrypt Sync Compare 2

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ console.log(result);
 
 
 
-
+app.listen(process.env.PORT || 3000, () => {});
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log("Listening on port:", PORT)
