5fc83613d3d96bfbb18e26f021377b1cafd265b6
mtc bamalam <mc_bamalam@protonmail.com>
2024-04-06 14:38:41 +0000

    Bcrypt Sync Compare

--- server.js
+++ server.js
@@ -3,15 +3,14 @@ const express     = require('express');
 const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
 const app         = express();
+exports.app = app;
 const bcrypt      = require('bcrypt');
 fccTesting(app);
 const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
 
-app.get('/", (reg, res) => {
-    res.send{"Hello World!");
-});
+
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
     console.log(hash);
